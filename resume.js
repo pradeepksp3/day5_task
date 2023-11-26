9190e326f141d88d063b007e7f306a701be9016e
pradeepksp3 <151113169+pradeepksp3@users.noreply.github.com>
2023-11-26 17:11:17 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,35 +1,37 @@
-`{
-Name: 'Pradeepkumar k',
-    Age: 27,
-        githubid: 'pradeepkumarksp',
-            contactdetails: {
-    mail: 'pradeepkumarksp62gmail.com',
-        Phonenumber: 959746313,
-            Address: 'mariyamman kovil street \n' +
-                '                maravadi post\n' +
-                '                dharmapuri\n' +
-                '                635205.'
+[{
+"Name": "Pradeepkumar k",
+    "Age": "27",
+        "githubid": "pradeepkumarksp",
+            "contactdetails": {
+    "mail": "pradeepkumarksp6@gmail.com",
+        "Phonenumber": "959746313",
+            "Address": "mariyamman kovil street"
+                '                "maravadi post"
+                '                "dharmapuri"
+                '                "635205"
 },
-Objective: 'To obtain resposible position in an origanization with best capabilities\n' +
-    '        skills and knowledge',
-    Skills: 'javascript full stock developer',
-        Workexperience: 'Fresher',
-            Academic: {
-    school: {
-        sslc: 'Govt HS school\n                        mark - 417',
-            HS: 'Govt HS school\n                    mark - 1050'
+"Objective": "To obtain resposible position in an origanization with best capabilities"
+    '        "skills and knowledge",
+    "Skills": "javascript full stock developer",
+        "Workexperience": "Fresher",
+            "Academic": {
+    "school": {
+        "sslc": "Govt HS school"
+            "mark - 417",
+            "HS": "Govt HS school"
+                "mark" - "1050"
     },
-    degree: {
-        title: 'bsc agriculture',
-            collage: 'kumaraguru institute of agriculture',
-                passedoutyear: 2018,
-                    Ogpa: 8
+    "degree": {
+        "title": "bsc agriculture",
+            "collage": "kumaraguru institute of agriculture",
+                "passedoutyear": "2018",
+                    "Ogpa": "8.0"
     }
 },
-projet: {
-    tittle: 'Framer producer organization',
-        duration: '6months',
-            Extracaricularactivities: 'vollyball, cicket, kabadi',
-                hobbies: 'playing games, listening music, watching movies'
+"projet": {
+    "tittle": "Framer producer organization",
+        "duration": "6months",
+            "Extracaricularactivities": "vollyball, cicket, kabadi",
+                "hobbies": "playing games, listening music, watching movies"
 }
-}`
+}]
